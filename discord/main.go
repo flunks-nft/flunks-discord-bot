313b632904ed7268a564982090d52e0812e4d162
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-07-04 18:26:12 -0400

    Add vaniella slash command

--- discord/main.go
+++ discord/main.go
@@ -1,7 +1,9 @@
 package discord
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -21,22 +23,46 @@ var (
 	RAID_CHANNEL_ID     string
 	RAID_LOG_CHANNEL_ID string
 
-	GUI_ID *string
+	GuildID *string
 
 	dg                  *discordgo.Session
 	messageHandlers     []func(s *discordgo.Session, i *discordgo.MessageCreate)
 	interactionHandlers []func(s *discordgo.Session, i *discordgo.InteractionCreate)
+
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name: "basic-command",
+			// All commands and options must have a description
+			// Commands/options without description will fail the registration
+			// of the command.
+			Description: "Basic command",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Hey there! Congratulations, you just executed your first slash command",
+				},
+			})
+		},
+	}
+
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
 )
 
 func init() {
 	utils.LoadEnv()
+	flag.Parse()
 
 	DISCORD_TOKEN = os.Getenv("DISCORD_TOKEN")
 	RAID_CHANNEL_ID = os.Getenv("RAID_CHANNLE_ID")
 	RAID_LOG_CHANNEL_ID = os.Getenv("RAID_LOG_CHANNEL_ID")
 	DISCORD_TOKEN = os.Getenv("DISCORD_TOKEN")
 
-	GUI_ID = DefaultGuiID()
+	GuildID = DefaultGuiID()
 
 	messageHandlers = []func(s *discordgo.Session, i *discordgo.MessageCreate){
 		FlowAddressHandler,
@@ -49,6 +75,7 @@ func init() {
 }
 
 func InitDiscord() {
+
 	// Create a new Discord session using the provided bot token.
 	s, err := discordgo.New("Bot " + DISCORD_TOKEN)
 	if err != nil {
@@ -67,6 +94,16 @@ func InitDiscord() {
 		dg.AddHandler(handler)
 	}
 
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+
 	// we only care about receiving message events
 	s.Identify.Intents = discordgo.IntentsGuildMessages
 
@@ -77,12 +114,43 @@ func InitDiscord() {
 		return
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
 
 	// Cleanly close down the Discord session.
-	s.Close()
+	defer s.Close()
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+	if *RemoveCommands {
+		log.Println("Removing commands...")
+		// // We need to fetch the commands, since deleting requires the command ID.
+		// // We are doing this from the returned commands on line 375, because using
+		// // this will delete all the commands, which might not be desirable, so we
+		// // are deleting only the commands that we added.
+		// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
+		// if err != nil {
+		// 	log.Fatalf("Could not fetch registered commands: %v", err)
+		// }
+
+		for _, v := range registeredCommands {
+			err := s.ApplicationCommandDelete(s.State.User.ID, *GuildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+	log.Println("Gracefully shutting down.")
 }
