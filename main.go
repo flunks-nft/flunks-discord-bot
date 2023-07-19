0c97698a7bc1c1b6c71c60365c86384a58735c94
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-07-19 18:24:12 -0400

    Misc: cleanup

--- main.go
+++ main.go
@@ -1,37 +1,8 @@
 package main
 
 import (
-	"github.com/bwmarrin/discordgo"
 	"github.com/flunks-nft/discord-bot/db"
 	"github.com/flunks-nft/discord-bot/discord"
-)
-
-var (
-	discordToken   string
-	DISCORD_GUI_ID string
-
-	GuildID = ""
-
-	commands = []*discordgo.ApplicationCommand{
-		{
-			Name: "basic-command",
-			// All commands and options must have a description
-			// Commands/options without description will fail the registration
-			// of the command.
-			Description: "Basic command",
-		},
-	}
-
-	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: "Hey there! Congratulations, you just executed your first slash command",
-				},
-			})
-		},
-	}
 )
 
 func main() {
