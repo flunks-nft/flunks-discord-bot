54999cc266c8a64bb845487bc83e79e50fd7c348
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-08-01 21:44:17 -0400

    preload fromNft and ToNft so points are added properly

--- pkg/db/raid.go
+++ pkg/db/raid.go
@@ -94,7 +94,7 @@ func ConcludeOneRaid() (raid Raid, err error) {
 	}()
 
 	// Find the 1 raid where createdAt is more than <RAID_CONCLUDE_TIME> hours ago and is the oldest, and IsConcluded is false
-	result := tx.Where("is_concluded = ? AND created_at < ?", false, time.Now().UTC().Add(-RAID_CONCLUDE_TIME)).Order("created_at ASC").First(&raid)
+	result := tx.Preload("FromNft").Preload("ToNft").Where("is_concluded = ? AND created_at < ?", false, time.Now().UTC().Add(-RAID_CONCLUDE_TIME)).Order("created_at ASC").First(&raid)
 	if result.Error != nil {
 		return raid, result.Error
 	}
