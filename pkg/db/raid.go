d4bdad27f61fa5b229967de8c57a8bf263e0e785
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-08-23 16:44:18 -0400

    Misc: fix missing bracket

--- pkg/db/raid.go
+++ pkg/db/raid.go
@@ -35,6 +35,7 @@ func getRandomChallengeType() ChallengeType {
 
 var (
 	RAID_CONCLUDE_INTERVAL_IN_SECONDS time.Duration
+	RAID_CONCLUDE_INTERVAL            time.Duration
 
 	// TODO: replace them with the real emojis
 	RAID_WON_EMOJI_ID  = utils.DiscordEmojis["RAID_WON_EMOJI_ID"]
@@ -103,7 +104,7 @@ func ConcludeOneRaid() (raid Raid, err error) {
 	}()
 
 	// Find the 1 raid where createdAt is more than <RAID_CONCLUDE_TIME> hours ago and is the oldest, and IsConcluded is false
-	result := tx.Preload("FromNft").Preload("ToNft").Where("is_concluded = ? AND created_at < ?", false, time.Now().UTC().Add(-RAID_CONCLUDE_INTERVAL_IN_SECONDS).Order("created_at ASC").First(&raid)
+	result := tx.Preload("FromNft").Preload("ToNft").Where("is_concluded = ? AND created_at < ?", false, time.Now().UTC().Add(-RAID_CONCLUDE_INTERVAL_IN_SECONDS)).Order("created_at ASC").First(&raid)
 	if result.Error != nil {
 		return raid, result.Error
 	}
