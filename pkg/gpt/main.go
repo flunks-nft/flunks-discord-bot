79b4aae731f69f393edd4e61d32e9e00533424a0
0xHippo <37877455+alliu930410@users.noreply.github.com>
2023-09-21 16:30:21 -0400

    Bold Flunks in prompt response

--- pkg/gpt/main.go
+++ pkg/gpt/main.go
@@ -36,24 +36,25 @@ func GenerateBattlePrompt(clique string, challenger, defender uint) string {
 	Put the winner in a winner variable in the json object, 0 when challenger's actionOutcome is positive and 1 when challenger's actionOutcome is negative.
 	Make sure the weapon picked fits the clique of a %s battle in a high school setup.
 	Make sure the wording fits the style in the example provided below.
+	Make sure to bold all the Flunk #%d and Flunk #%d in the example provided below with **<content>**.
 	Generate a JSON representation for a 'BattleLog' that includes fields 'Action', 'ActionOutcome', and 'BattleOutcome'.
 	Example:
 	Positive payload:
 {
-	"weapon": "Look at those gains! Flunk #%d is getting prepped with a Protein Shake.",
-	"action": "Flunk #%d finished the protein shake.",
-	"actionOutcome": "That should not be allowed in school, Flunk #%d has turned green after taking the protein shake and SMASHES Flunk #%d to smithereens.",
-	"battleOutcome": "Flunk #%d emerges victorious, crushing Flunk #%d in the battle!",
+	"weapon": "Look at those gains! **Flunk #%d** is getting prepped with a Protein Shake.",
+	"action": "**Flunk #%d** finished the protein shake.",
+	"actionOutcome": "That should not be allowed in school, **Flunk #%d** has turned green after taking the protein shake and SMASHES **Flunk #%d** to smithereens.",
+	"battleOutcome": "**Flunk #%d** emerges victorious, crushing **Flunk #%d** in the battle!",
 	"winner": 0,
 }
 Negative payload:
 {
-	"weapon": "Look at those gains! Flunk #%d is getting prepped with a Protein Shake.",
-	"action": "Flunk #%d finished the protein shake.",
-	"actionOutcome": "Flunk #%d downed the lumpy-ass protein shake in one, then flexed his muscles so much they passed out",
-	"battleOutcome": "What a shame! Flunk #%d won the battle with no effort!",
+	"weapon": "Look at those gains! **Flunk #%d** is getting prepped with a Protein Shake.",
+	"action": "**Flunk #%d** finished the protein shake.",
+	"actionOutcome": "**Flunk #%d** downed the lumpy-ass protein shake in one, then flexed his muscles so much they passed out",
+	"battleOutcome": "What a shame! **Flunk #%d** won the battle with no effort!",
 	"winner": 0,
 }
 
-	`, challenger, defender, clique, challenger, challenger, challenger, defender, challenger, defender, challenger, challenger, challenger, defender)
+	`, challenger, defender, clique, challenger, defender, challenger, challenger, challenger, defender, challenger, defender, challenger, challenger, challenger, defender)
 }
